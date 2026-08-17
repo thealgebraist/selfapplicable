2bc2e9ce60e77faeb63e4c8cbd76dce757a5bc3f
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 10:01:43 +0200

    Add epoll pwait query primitive

---
+++ fixtures/epoll_pwait_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  epoll_pwait_query();
+  return 0;
+}
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -311,6 +311,7 @@ expect_status fixtures/fcntl_getfl_stdout.c 0
 expect_status fixtures/fcntl_getown_stdout.c 0
 expect_status fixtures/fcntl_getsig_stdout.c 0
 expect_status fixtures/epoll_ctl_query.c 0
+expect_status fixtures/epoll_pwait_query.c 0
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
