e1745e7c0c9e5cb9b9746a07e2205e7b23d7931f
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 08:15:49 +0200

    Add sched affinity primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -22,7 +22,7 @@ std::string read_source(const char *path) {
   return all;
 }
 
-struct Program { int argc_value=-1, then_status=0, else_status=0, switch_case=-1, switch_case_status=0, switch_case2=-1, switch_case2_status=0, switch_default_status=0; std::string output, error_output, loop_output, directory, filter, exists_path, directory_path, regular_path, size_path, cat_path, mkdir_path, rm_path, rmdir_path, touch_path, chdir_path, symlink_target, symlink_path, link_old, link_new, readlink_path, rename_old, rename_new, chmod_path, access_path, truncate_path, fsync_path, fdatasync_path, writefd_text, stat_path, lstat_path; std::vector<std::pair<int,std::string>> ordered_output; unsigned long long size_bytes=0, truncate_size=0, random_bytes=0, stdin_bytes=0, sleep_seconds=0, umask_mode=0, alarm_seconds=0, clock_id=0, rusage_who=0, rlimit_resource=0, priority_query_which=0, priority_query_who=0; unsigned chmod_mode=0, access_mode=0, dup_old=0, dup_new=0, close_fd=0, tty_fd=0, fcntl_fd=0, fcntl_cmd=0, fcntl_arg=0, setpgid_pid=0, setpgid_pgid=0, priority_which=0, priority_who=0, priority_value=0, nice_increment=0, writefd_fd=0, writefd_len=0, readfd_fd=0, readfd_len=0, poll_fd=0, poll_events=0, poll_timeout=0, fstat_fd=0; int loop_count=0; bool loop_present=false, loop_inclusive=false, loop_do=false, argv1=false, arg_help=false, cwd=false, listdir=false, cat=false, mkdir=false, rm=false, rmdir=false, touch=false, chdir=false, symlink=false, link=false, readlink=false, rename=false, chmod=false, access=false, truncate=false, getrandom=false, readstdin=false, sleep=false, isatty=false, sync=false, fsync=false, fdatasync=false, umask=false, fcntl=false, setpgid=false, yield=false, getpid=false, getppid=false, setpriority=false, isroot=false, gettid=false, isgroup0=false, ise_group0=false, nice=false, writefd=false, readfd=false, poll=false, alarm=false, clock_gettime=false, gettimeofday=false, times=false, getrusage=false, sysinfo=false, uname=false, getdomainname=false, fstat=false, stat=false, lstat=false, getgroups=false, getresuid=false, getresgid=false, getrlimit=false, getpriority=false, getcpu=false, dup=false, close=false, pipe=false, exists=false, is_directory=false, is_regular=false, size_gt=false, function_call=false, null_guard=false, pointer_equal=false, switch_return=false, switch_two_cases=false; };
+struct Program { int argc_value=-1, then_status=0, else_status=0, switch_case=-1, switch_case_status=0, switch_case2=-1, switch_case2_status=0, switch_default_status=0; std::string output, error_output, loop_output, directory, filter, exists_path, directory_path, regular_path, size_path, cat_path, mkdir_path, rm_path, rmdir_path, touch_path, chdir_path, symlink_target, symlink_path, link_old, link_new, readlink_path, rename_old, rename_new, chmod_path, access_path, truncate_path, fsync_path, fdatasync_path, writefd_text, stat_path, lstat_path; std::vector<std::pair<int,std::string>> ordered_output; unsigned long long size_bytes=0, truncate_size=0, random_bytes=0, stdin_bytes=0, sleep_seconds=0, umask_mode=0, alarm_seconds=0, clock_id=0, rusage_who=0, rlimit_resource=0, priority_query_which=0, priority_query_who=0, affinity_pid=0; unsigned chmod_mode=0, access_mode=0, dup_old=0, dup_new=0, close_fd=0, tty_fd=0, fcntl_fd=0, fcntl_cmd=0, fcntl_arg=0, setpgid_pid=0, setpgid_pgid=0, priority_which=0, priority_who=0, priority_value=0, nice_increment=0, writefd_fd=0, writefd_len=0, readfd_fd=0, readfd_len=0, poll_fd=0, poll_events=0, poll_timeout=0, fstat_fd=0; int loop_count=0; bool loop_present=false, loop_inclusive=false, loop_do=false, argv1=false, arg_help=false, cwd=false, listdir=false, cat=false, mkdir=false, rm=false, rmdir=false, touch=false, chdir=false, symlink=false, link=false, readlink=false, rename=false, chmod=false, access=false, truncate=false, getrandom=false, readstdin=false, sleep=false, isatty=false, sync=false, fsync=false, fdatasync=false, umask=false, fcntl=false, setpgid=false, yield=false, getpid=false, getppid=false, setpriority=false, isroot=false, gettid=false, isgroup0=false, ise_group0=false, nice=false, writefd=false, readfd=false, poll=false, alarm=false, clock_gettime=false, gettimeofday=false, times=false, getrusage=false, sysinfo=false, uname=false, getdomainname=false, fstat=false, stat=false, lstat=false, getgroups=false, getresuid=false, getresgid=false, getrlimit=false, getpriority=false, getcpu=false, sched_getaffinity=false, dup=false, close=false, pipe=false, exists=false, is_directory=false, is_regular=false, size_gt=false, function_call=false, null_guard=false, pointer_equal=false, switch_return=false, switch_two_cases=false; };
 
 Program parse_main(std::string const& s) {
   std::smatch main_match;
@@ -1633,6 +1633,8 @@ Program parse_main(std::string const& s) {
   static const std::regex priority_query_call(R"re(getpriority\s*\(\s*([0-9]+)\s*,\s*([0-9]+)\s*\)\s*;)re");
   if(std::regex_search(body,d,priority_query_call)) { p.getpriority=true; p.priority_query_which=std::stoul(d[1]); p.priority_query_who=std::stoul(d[2]); if(p.priority_query_which>2) throw std::runtime_error("priority selector out of range"); }
   p.getcpu=std::regex_search(body,std::regex(R"re(\bgetcpu\s*\(\s*\)\s*;)re"));
+  static const std::regex affinity_call(R"re(sched_getaffinity\s*\(\s*([0-9]+)\s*\)\s*;)re");
+  if(std::regex_search(body,d,affinity_call)) { p.sched_getaffinity=true; p.affinity_pid=std::stoull(d[1]); }
   static const std::regex duplicate_fd(R"re(dup2\s*\(\s*([0-9]+)\s*,\s*([0-9]+)\s*\)\s*;)re");
   if(std::regex_search(body,d,duplicate_fd)) { p.dup=true; p.dup_old=static_cast<unsigned>(std::stoul(d[1])); p.dup_new=static_cast<unsigned>(std::stoul(d[2])); }
   static const std::regex close_fd_call(R"re(close\s*\(\s*([0-9]+)\s*\)\s*;)re");
@@ -1991,6 +1993,11 @@ void emit_getcpu(Program const&) {
     <<"  mov $309, %eax\n  lea cpu_buf(%rip), %rdi\n  lea cpu_node(%rip), %rsi\n  xor %edx, %edx\n  syscall\n  test %eax, %eax\n  js .Lgetcpu_fail\n  xor %edi, %edi\n  jmp .Lgetcpu_done\n.Lgetcpu_fail:\n  mov $1, %edi\n.Lgetcpu_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ncpu_buf:\n  .skip 4\ncpu_node:\n  .skip 4\n";
 }
 
+void emit_sched_getaffinity(Program const& p) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $204, %eax\n  mov $"<<p.affinity_pid<<", %edi\n  mov $8, %esi\n  lea affinity_buf(%rip), %rdx\n  syscall\n  test %eax, %eax\n  js .Laffinity_fail\n  xor %edi, %edi\n  jmp .Laffinity_done\n.Laffinity_fail:\n  mov $1, %edi\n.Laffinity_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\naffinity_buf:\n  .skip 8\n";
+}
+
 void emit_dup(Program const& p) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $33, %eax\n  mov $"<<p.dup_old<<", %edi\n  mov $"<<p.dup_new<<", %esi\n  syscall\n  test %eax, %eax\n  js .Ldup_fail\n  xor %edi, %edi\n  jmp .Ldup_done\n.Ldup_fail:\n  mov $1, %edi\n.Ldup_done:\n  mov $60, %eax\n  syscall\n";
@@ -2083,6 +2090,7 @@ int main(int argc,char **argv) {
     if(program.getrlimit) { csubset::emit_getrlimit(program); return 0; }
     if(program.getpriority) { csubset::emit_getpriority(program); return 0; }
     if(program.getcpu) { csubset::emit_getcpu(program); return 0; }
+    if(program.sched_getaffinity) { csubset::emit_sched_getaffinity(program); return 0; }
     if(program.dup) { csubset::emit_dup(program); return 0; }
     if(program.close) { csubset::emit_close(program); return 0; }
     if(program.pipe) { csubset::emit_pipe(program); return 0; }
---
+++ fixtures/sched_affinity_self.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  sched_getaffinity(0);
+  return 0;
+}
