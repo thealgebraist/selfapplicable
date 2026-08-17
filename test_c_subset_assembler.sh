6530f5725de73b68c672ab2c9c1e0cacc5f459e4
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 09:23:46 +0200

    Add readahead primitive

--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -289,6 +289,7 @@ expect_status fixtures/statfs_tmp.c 0
 expect_status fixtures/fstatfs_stdout.c 0
 expect_status fixtures/getdents64_tmp.c 0
 expect_status fixtures/copy_file_range_zero.c 1
+expect_status fixtures/readahead_stdout.c 1
 expect_stdin_output fixtures/readstdin_four.c ABCD ABCD
 expect_stdin_output fixtures/readstdin_eof_after_chunk.c ABCD ABCD
 expect_status fixtures/dup_stdout_stderr.c 0
