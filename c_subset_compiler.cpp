b465ec3b90ff78e190d596583698f442cc9d9459
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 07:32:41 +0200

    Add gettimeofday primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -22,7 +22,7 @@ std::string read_source(const char *path) {
   return all;
 }
 
-struct Program { int argc_value=-1, then_status=0, else_status=0, switch_case=-1, switch_case_status=0, switch_case2=-1, switch_case2_status=0, switch_default_status=0; std::string output, error_output, loop_output, directory, filter, exists_path, directory_path, regular_path, size_path, cat_path, mkdir_path, rm_path, rmdir_path, touch_path, chdir_path, symlink_target, symlink_path, link_old, link_new, readlink_path, rename_old, rename_new, chmod_path, access_path, truncate_path, fsync_path, fdatasync_path, writefd_text; std::vector<std::pair<int,std::string>> ordered_output; unsigned long long size_bytes=0, truncate_size=0, random_bytes=0, stdin_bytes=0, sleep_seconds=0, umask_mode=0, alarm_seconds=0, clock_id=0; unsigned chmod_mode=0, access_mode=0, dup_old=0, dup_new=0, close_fd=0, tty_fd=0, fcntl_fd=0, fcntl_cmd=0, fcntl_arg=0, setpgid_pid=0, setpgid_pgid=0, priority_which=0, priority_who=0, priority_value=0, nice_increment=0, writefd_fd=0, writefd_len=0, readfd_fd=0, readfd_len=0, poll_fd=0, poll_events=0, poll_timeout=0; int loop_count=0; bool loop_present=false, loop_inclusive=false, loop_do=false, argv1=false, arg_help=false, cwd=false, listdir=false, cat=false, mkdir=false, rm=false, rmdir=false, touch=false, chdir=false, symlink=false, link=false, readlink=false, rename=false, chmod=false, access=false, truncate=false, getrandom=false, readstdin=false, sleep=false, isatty=false, sync=false, fsync=false, fdatasync=false, umask=false, fcntl=false, setpgid=false, yield=false, getpid=false, getppid=false, setpriority=false, isroot=false, gettid=false, isgroup0=false, nice=false, writefd=false, readfd=false, poll=false, alarm=false, clock_gettime=false, dup=false, close=false, pipe=false, exists=false, is_directory=false, is_regular=false, size_gt=false, function_call=false, null_guard=false, pointer_equal=false, switch_return=false, switch_two_cases=false; };
+struct Program { int argc_value=-1, then_status=0, else_status=0, switch_case=-1, switch_case_status=0, switch_case2=-1, switch_case2_status=0, switch_default_status=0; std::string output, error_output, loop_output, directory, filter, exists_path, directory_path, regular_path, size_path, cat_path, mkdir_path, rm_path, rmdir_path, touch_path, chdir_path, symlink_target, symlink_path, link_old, link_new, readlink_path, rename_old, rename_new, chmod_path, access_path, truncate_path, fsync_path, fdatasync_path, writefd_text; std::vector<std::pair<int,std::string>> ordered_output; unsigned long long size_bytes=0, truncate_size=0, random_bytes=0, stdin_bytes=0, sleep_seconds=0, umask_mode=0, alarm_seconds=0, clock_id=0; unsigned chmod_mode=0, access_mode=0, dup_old=0, dup_new=0, close_fd=0, tty_fd=0, fcntl_fd=0, fcntl_cmd=0, fcntl_arg=0, setpgid_pid=0, setpgid_pgid=0, priority_which=0, priority_who=0, priority_value=0, nice_increment=0, writefd_fd=0, writefd_len=0, readfd_fd=0, readfd_len=0, poll_fd=0, poll_events=0, poll_timeout=0; int loop_count=0; bool loop_present=false, loop_inclusive=false, loop_do=false, argv1=false, arg_help=false, cwd=false, listdir=false, cat=false, mkdir=false, rm=false, rmdir=false, touch=false, chdir=false, symlink=false, link=false, readlink=false, rename=false, chmod=false, access=false, truncate=false, getrandom=false, readstdin=false, sleep=false, isatty=false, sync=false, fsync=false, fdatasync=false, umask=false, fcntl=false, setpgid=false, yield=false, getpid=false, getppid=false, setpriority=false, isroot=false, gettid=false, isgroup0=false, nice=false, writefd=false, readfd=false, poll=false, alarm=false, clock_gettime=false, gettimeofday=false, dup=false, close=false, pipe=false, exists=false, is_directory=false, is_regular=false, size_gt=false, function_call=false, null_guard=false, pointer_equal=false, switch_return=false, switch_two_cases=false; };
 
 Program parse_main(std::string const& s) {
   std::smatch main_match;
@@ -1610,6 +1610,7 @@ Program parse_main(std::string const& s) {
   if(std::regex_search(body,d,alarm_call)) { p.alarm=true; p.alarm_seconds=std::stoull(d[1]); if(p.alarm_seconds>3600) throw std::runtime_error("alarm too large"); }
   static const std::regex clock_call(R"re(clock_gettime\s*\(\s*([0-9]+)\s*\)\s*;)re");
   if(std::regex_search(body,d,clock_call)) { p.clock_gettime=true; p.clock_id=std::stoul(d[1]); if(p.clock_id>16) throw std::runtime_error("clock id out of range"); }
+  p.gettimeofday=std::regex_search(body,std::regex(R"re(\bgettimeofday\s*\(\s*\)\s*;)re"));
   static const std::regex duplicate_fd(R"re(dup2\s*\(\s*([0-9]+)\s*,\s*([0-9]+)\s*\)\s*;)re");
   if(std::regex_search(body,d,duplicate_fd)) { p.dup=true; p.dup_old=static_cast<unsigned>(std::stoul(d[1])); p.dup_new=static_cast<unsigned>(std::stoul(d[2])); }
   static const std::regex close_fd_call(R"re(close\s*\(\s*([0-9]+)\s*\)\s*;)re");
@@ -1888,6 +1889,11 @@ void emit_clock_gettime(Program const& p) {
     <<"  mov $228, %eax\n  mov $"<<p.clock_id<<", %edi\n  lea clock_ts(%rip), %rsi\n  syscall\n  test %eax, %eax\n  js .Lclock_fail\n  xor %edi, %edi\n  jmp .Lclock_done\n.Lclock_fail:\n  mov $1, %edi\n.Lclock_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\nclock_ts:\n  .skip 16\n";
 }
 
+void emit_gettimeofday(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $96, %eax\n  lea timeval(%rip), %rdi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Lgettimeofday_fail\n  xor %edi, %edi\n  jmp .Lgettimeofday_done\n.Lgettimeofday_fail:\n  mov $1, %edi\n.Lgettimeofday_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimeval:\n  .skip 16\n";
+}
+
 void emit_dup(Program const& p) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $33, %eax\n  mov $"<<p.dup_old<<", %edi\n  mov $"<<p.dup_new<<", %esi\n  syscall\n  test %eax, %eax\n  js .Ldup_fail\n  xor %edi, %edi\n  jmp .Ldup_done\n.Ldup_fail:\n  mov $1, %edi\n.Ldup_done:\n  mov $60, %eax\n  syscall\n";
@@ -1964,6 +1970,7 @@ int main(int argc,char **argv) {
     if(program.poll) { csubset::emit_poll(program); return 0; }
     if(program.alarm) { csubset::emit_alarm(program); return 0; }
     if(program.clock_gettime) { csubset::emit_clock_gettime(program); return 0; }
+    if(program.gettimeofday) { csubset::emit_gettimeofday(program); return 0; }
     if(program.dup) { csubset::emit_dup(program); return 0; }
     if(program.close) { csubset::emit_close(program); return 0; }
     if(program.pipe) { csubset::emit_pipe(program); return 0; }
