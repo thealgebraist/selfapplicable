2b5338ab3d23b213e427a6c04f3b592f9957677d
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:30:15 +0200

    Add sigaltstack query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -307,6 +307,7 @@ bool emit_vmsplice_query_mode = false;
 bool emit_pidfd_open_query_mode = false;
 bool emit_pidfd_send_signal_query_mode = false;
 bool emit_pidfd_getfd_query_mode = false;
+bool emit_sigaltstack_query_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -2189,6 +2190,7 @@ Program parse_main(std::string const& s) {
   emit_pidfd_open_query_mode=std::regex_search(body,std::regex(R"re(\bpidfd_open_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_send_signal_query_mode=std::regex_search(body,std::regex(R"re(\bpidfd_send_signal_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_query_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_query\s*\(\s*\)\s*;)re"));
+  emit_sigaltstack_query_mode=std::regex_search(body,std::regex(R"re(\bsigaltstack_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -4076,6 +4078,15 @@ void emit_pidfd_getfd_query(Program const&) {
     <<"  mov $60, %eax\n  syscall\n";
 }
 
+void emit_sigaltstack_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $131, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lsigaltstack_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lsigaltstack_done\n"
+    <<".Lsigaltstack_fail:\n  mov $1, %edi\n.Lsigaltstack_done:\n"
+    <<"  mov $60, %eax\n  syscall\n";
+}
+
 void emit_getrandom_query(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $318, %eax\n  lea getrandom_buffer(%rip), %rdi\n  mov $16, %esi\n  xor %edx, %edx\n  syscall\n"
@@ -5053,6 +5064,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_pidfd_open_query_mode) { csubset::emit_pidfd_open_query(program); return 0; }
     if(csubset::emit_pidfd_send_signal_query_mode) { csubset::emit_pidfd_send_signal_query(program); return 0; }
     if(csubset::emit_pidfd_getfd_query_mode) { csubset::emit_pidfd_getfd_query(program); return 0; }
+    if(csubset::emit_sigaltstack_query_mode) { csubset::emit_sigaltstack_query(program); return 0; }
     if(csubset::emit_epoll_wait_mode) { csubset::emit_epoll_wait(program); return 0; }
     if(csubset::emit_futex_wait_mode) { csubset::emit_futex_wait(program); return 0; }
     if(csubset::emit_timerfd_gettime_mode) { csubset::emit_timerfd_gettime(program); return 0; }
---
+++ fixtures/sigaltstack_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  sigaltstack_query();
+  return 0;
+}
