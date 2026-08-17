dc60bcf1637b74f94b8f363575782b64c617f907
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:34:18 +0200

    Add vmsplice query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -232,6 +232,7 @@ bool emit_sendfile_query_mode = false;
 bool emit_copy_file_range_query_mode = false;
 bool emit_splice_query_mode = false;
 bool emit_tee_query_mode = false;
+bool emit_vmsplice_query_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -2039,6 +2040,7 @@ Program parse_main(std::string const& s) {
   emit_copy_file_range_query_mode=std::regex_search(body,std::regex(R"re(\bcopy_file_range_query\s*\(\s*\)\s*;)re"));
   emit_splice_query_mode=std::regex_search(body,std::regex(R"re(\bsplice_query\s*\(\s*\)\s*;)re"));
   emit_tee_query_mode=std::regex_search(body,std::regex(R"re(\btee_query\s*\(\s*\)\s*;)re"));
+  emit_vmsplice_query_mode=std::regex_search(body,std::regex(R"re(\bvmsplice_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -3892,6 +3894,14 @@ void emit_tee_query(Program const&) {
     <<".Ltee_fail:\n  mov $1, %edi\n.Ltee_done:\n  mov $60, %eax\n  syscall\n";
 }
 
+void emit_vmsplice_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $278, %eax\n  mov $1, %edi\n  xor %esi, %esi\n  xor %edx, %edx\n  xor %r10d, %r10d\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lvmsplice_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lvmsplice_done\n"
+    <<".Lvmsplice_fail:\n  mov $1, %edi\n.Lvmsplice_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -4221,6 +4231,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_copy_file_range_query_mode) { csubset::emit_copy_file_range_query(program); return 0; }
     if(csubset::emit_splice_query_mode) { csubset::emit_splice_query(program); return 0; }
     if(csubset::emit_tee_query_mode) { csubset::emit_tee_query(program); return 0; }
+    if(csubset::emit_vmsplice_query_mode) { csubset::emit_vmsplice_query(program); return 0; }
     if(csubset::emit_epoll_wait_mode) { csubset::emit_epoll_wait(program); return 0; }
     if(csubset::emit_futex_wait_mode) { csubset::emit_futex_wait(program); return 0; }
     if(csubset::emit_timerfd_gettime_mode) { csubset::emit_timerfd_gettime(program); return 0; }
---
+++ fixtures/vmsplice_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  vmsplice_query();
+  return 0;
+}
