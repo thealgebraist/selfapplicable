a28bb0b0836b8b211b5ddcc26d8bf6fc231ea8bc
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:48:51 +0200

    Add futex lock pi2 query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -118,6 +118,7 @@ bool emit_futex_wait_requeue_pi_query_mode = false;
 bool emit_futex_wait_bitset_query_mode = false;
 bool emit_futex_wake_bitset_query_mode = false;
 bool emit_timer_getoverrun_query_mode = false;
+bool emit_futex_lock_pi2_query_mode = false;
 bool emit_lsm_list_modules_query_mode = false;
 bool emit_lsm_set_self_attr_query_mode = false;
 bool emit_open_tree_query_mode = false;
@@ -2027,6 +2028,7 @@ Program parse_main(std::string const& s) {
   emit_futex_wait_bitset_query_mode=std::regex_search(body,std::regex(R"re(\bfutex_wait_bitset_query\s*\(\s*\)\s*;)re"));
   emit_futex_wake_bitset_query_mode=std::regex_search(body,std::regex(R"re(\bfutex_wake_bitset_query\s*\(\s*\)\s*;)re"));
   emit_timer_getoverrun_query_mode=std::regex_search(body,std::regex(R"re(\btimer_getoverrun_query\s*\(\s*\)\s*;)re"));
+  emit_futex_lock_pi2_query_mode=std::regex_search(body,std::regex(R"re(\bfutex_lock_pi2_query\s*\(\s*\)\s*;)re"));
   emit_lsm_list_modules_query_mode=std::regex_search(body,std::regex(R"re(\blsm_list_modules_query\s*\(\s*\)\s*;)re"));
   emit_lsm_set_self_attr_query_mode=std::regex_search(body,std::regex(R"re(\blsm_set_self_attr_query\s*\(\s*\)\s*;)re"));
   emit_open_tree_query_mode=std::regex_search(body,std::regex(R"re(\bopen_tree_query\s*\(\s*\)\s*;)re"));
@@ -3093,6 +3095,15 @@ void emit_timer_getoverrun_query(Program const&) {
     <<"  xor %edi, %edi\n  jmp .Ltimer_getoverrun_done\n"
     <<".Ltimer_getoverrun_fail:\n  mov $1, %edi\n.Ltimer_getoverrun_done:\n"
     <<"  mov $60, %eax\n  syscall\n";
+}
+
+void emit_futex_lock_pi2_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $202, %eax\n  lea futex_lock_pi2_word(%rip), %rdi\n  mov $13, %esi\n  xor %edx, %edx\n  lea futex_lock_pi2_ts(%rip), %r10\n  xor %r8d, %r8d\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lfutex_lock_pi2_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lfutex_lock_pi2_done\n"
+    <<".Lfutex_lock_pi2_fail:\n  mov $1, %edi\n.Lfutex_lock_pi2_done:\n"
+    <<"  mov $60, %eax\n  syscall\n.bss\n.align 4\nfutex_lock_pi2_word:\n  .skip 4\n.align 8\nfutex_lock_pi2_ts:\n  .skip 16\n";
 }
 
 void emit_lsm_list_modules_query(Program const&) {
@@ -5419,6 +5430,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_futex_wait_bitset_query_mode) { csubset::emit_futex_wait_bitset_query(program); return 0; }
     if(csubset::emit_futex_wake_bitset_query_mode) { csubset::emit_futex_wake_bitset_query(program); return 0; }
     if(csubset::emit_timer_getoverrun_query_mode) { csubset::emit_timer_getoverrun_query(program); return 0; }
+    if(csubset::emit_futex_lock_pi2_query_mode) { csubset::emit_futex_lock_pi2_query(program); return 0; }
     if(csubset::emit_lsm_list_modules_query_mode) { csubset::emit_lsm_list_modules_query(program); return 0; }
     if(csubset::emit_lsm_set_self_attr_query_mode) { csubset::emit_lsm_set_self_attr_query(program); return 0; }
     if(csubset::emit_open_tree_query_mode) { csubset::emit_open_tree_query(program); return 0; }
