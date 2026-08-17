0654c11d156ab5243524dafe96fac924cbbc2ffe
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:23:22 +0200

    Add dup query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -214,6 +214,7 @@ bool emit_lstat_query_mode = false;
 bool emit_getdents_query_mode = false;
 bool emit_open_query_mode = false;
 bool emit_close_query_mode = false;
+bool emit_dup_query_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -2003,6 +2004,7 @@ Program parse_main(std::string const& s) {
   emit_getdents_query_mode=std::regex_search(body,std::regex(R"re(\bgetdents_query\s*\(\s*\)\s*;)re"));
   emit_open_query_mode=std::regex_search(body,std::regex(R"re(\bopen_query\s*\(\s*\)\s*;)re"));
   emit_close_query_mode=std::regex_search(body,std::regex(R"re(\bclose_query\s*\(\s*\)\s*;)re"));
+  emit_dup_query_mode=std::regex_search(body,std::regex(R"re(\bdup_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -3706,6 +3708,14 @@ void emit_close_query(Program const&) {
     <<".Lclose_fail:\n  mov $1, %edi\n.Lclose_done:\n  mov $60, %eax\n  syscall\n";
 }
 
+void emit_dup_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $32, %eax\n  mov $1, %edi\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Ldup_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Ldup_done\n"
+    <<".Ldup_fail:\n  mov $1, %edi\n.Ldup_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -4171,6 +4181,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_getdents_query_mode) { csubset::emit_getdents_query(program); return 0; }
     if(csubset::emit_open_query_mode) { csubset::emit_open_query(program); return 0; }
     if(csubset::emit_close_query_mode) { csubset::emit_close_query(program); return 0; }
+    if(csubset::emit_dup_query_mode) { csubset::emit_dup_query(program); return 0; }
     if(csubset::emit_timerfd_settime_mode) { csubset::emit_timerfd_settime(program); return 0; }
     if(csubset::emit_signalfd4_mode) { csubset::emit_signalfd4(program); return 0; }
     if(csubset::emit_pidfd_getfd_mode) { csubset::emit_pidfd_getfd(program); return 0; }
