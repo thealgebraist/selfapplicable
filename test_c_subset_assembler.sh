bbc2cf74098f98fd93c94b99694dfca7e19503fd
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 10:55:41 +0200

    Add sched setattr query primitive

--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -388,6 +388,7 @@ expect_status fixtures/quotactl_fd_query.c 1
 expect_status fixtures/landlock_add_rule_query.c 1
 expect_status fixtures/landlock_restrict_self_query.c 1
 expect_status fixtures/keyctl_query.c 1
+expect_status fixtures/sched_setattr_query.c 1
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
