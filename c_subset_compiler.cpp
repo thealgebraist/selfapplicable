6603d002144efc914285a95bf8fba991176eedba
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:39:23 +0200

    Add clock nanosleep query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -123,6 +123,7 @@ bool emit_process_vm_writev_query_mode = false;
 bool emit_getrandom_query_mode = false;
 bool emit_sched_yield_query_mode = false;
 bool emit_nanosleep_query_mode = false;
+bool emit_clock_nanosleep_query_mode = false;
 bool emit_clone3_query_mode = false;
 bool emit_userfaultfd_query_mode = false;
 bool emit_kcmp_query_mode = false;
@@ -1936,6 +1937,7 @@ Program parse_main(std::string const& s) {
   emit_getrandom_query_mode=std::regex_search(body,std::regex(R"re(\bgetrandom_query\s*\(\s*\)\s*;)re"));
   emit_sched_yield_query_mode=std::regex_search(body,std::regex(R"re(\bsched_yield_query\s*\(\s*\)\s*;)re"));
   emit_nanosleep_query_mode=std::regex_search(body,std::regex(R"re(\bnanosleep_query\s*\(\s*\)\s*;)re"));
+  emit_clock_nanosleep_query_mode=std::regex_search(body,std::regex(R"re(\bclock_nanosleep_query\s*\(\s*\)\s*;)re"));
   emit_clone3_query_mode=std::regex_search(body,std::regex(R"re(\bclone3_query\s*\(\s*\)\s*;)re"));
   emit_userfaultfd_query_mode=std::regex_search(body,std::regex(R"re(\buserfaultfd_query\s*\(\s*\)\s*;)re"));
   emit_kcmp_query_mode=std::regex_search(body,std::regex(R"re(\bkcmp_query\s*\(\s*\)\s*;)re"));
@@ -3953,6 +3955,14 @@ void emit_nanosleep_query(Program const&) {
     <<".Lnanosleep_fail:\n  mov $1, %edi\n.Lnanosleep_done:\n  mov $60, %eax\n  syscall\n";
 }
 
+void emit_clock_nanosleep_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $230, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  xor %edx, %edx\n  xor %r10d, %r10d\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lclock_nanosleep_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lclock_nanosleep_done\n"
+    <<".Lclock_nanosleep_fail:\n  mov $1, %edi\n.Lclock_nanosleep_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -4341,6 +4351,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_getrandom_query_mode) { csubset::emit_getrandom_query(program); return 0; }
     if(csubset::emit_sched_yield_query_mode) { csubset::emit_sched_yield_query(program); return 0; }
     if(csubset::emit_nanosleep_query_mode) { csubset::emit_nanosleep_query(program); return 0; }
+    if(csubset::emit_clock_nanosleep_query_mode) { csubset::emit_clock_nanosleep_query(program); return 0; }
     if(csubset::emit_process_vm_writev_query_mode) { csubset::emit_process_vm_writev_query(program); return 0; }
     if(csubset::emit_clone3_query_mode) { csubset::emit_clone3_query(program); return 0; }
     if(csubset::emit_userfaultfd_query_mode) { csubset::emit_userfaultfd_query(program); return 0; }
---
+++ fixtures/clock_nanosleep_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  clock_nanosleep_query();
+  return 0;
+}
