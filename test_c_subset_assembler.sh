bca14c09fdd3a649e0d760238836e3df48985a35
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 13:50:35 +0200

    Add mkdirat query primitive

--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -509,6 +509,7 @@ expect_status fixtures/readahead_query.c 1
 expect_status fixtures/close_range_query.c 1
 expect_status fixtures/getdents64_query.c 1
 expect_status fixtures/openat_query.c 1
+expect_status fixtures/mkdirat_query.c 1
 expect_status fixtures/landlock_add_rule_query.c 1
 expect_status fixtures/landlock_restrict_self_query.c 1
 expect_status fixtures/keyctl_query.c 1
