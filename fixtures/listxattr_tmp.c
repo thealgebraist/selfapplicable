e4819e584b935a1fe9dbf07cbd98b3c02dfddfcf
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 09:03:31 +0200

    Add listxattr primitive

---
+++ fixtures/listxattr_tmp.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  listxattr("/tmp");
+  return 0;
+}
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -273,6 +273,7 @@ expect_status fixtures/prctl_get_thp_disable.c 0
 expect_status fixtures/prctl_get_mce_kill.c 0
 expect_status fixtures/capget.c 0
 expect_status fixtures/statx_tmp.c 0
+expect_status fixtures/listxattr_tmp.c 0
 expect_stdin_output fixtures/readstdin_four.c ABCD ABCD
 expect_stdin_output fixtures/readstdin_eof_after_chunk.c ABCD ABCD
 expect_status fixtures/dup_stdout_stderr.c 0
