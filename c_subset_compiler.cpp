d9771412ee37a683d7e6a6f741d1b935a6f43c50
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:15:12 +0200

    Add chdir query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -199,6 +199,7 @@ bool emit_setpgid_query_mode = false;
 bool emit_setsid_query_mode = false;
 bool emit_umask_query_mode = false;
 bool emit_getcwd_query_mode = false;
+bool emit_chdir_query_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -1973,6 +1974,7 @@ Program parse_main(std::string const& s) {
   emit_setsid_query_mode=std::regex_search(body,std::regex(R"re(\bsetsid_query\s*\(\s*\)\s*;)re"));
   emit_umask_query_mode=std::regex_search(body,std::regex(R"re(\bumask_query\s*\(\s*\)\s*;)re"));
   emit_getcwd_query_mode=std::regex_search(body,std::regex(R"re(\bgetcwd_query\s*\(\s*\)\s*;)re"));
+  emit_chdir_query_mode=std::regex_search(body,std::regex(R"re(\bchdir_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -3540,6 +3542,15 @@ void emit_getcwd_query(Program const&) {
     <<".bss\n.align 8\ngetcwd_buffer:\n  .skip 256\n";
 }
 
+void emit_chdir_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $80, %eax\n  lea chdir_path(%rip), %rdi\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lchdir_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lchdir_done\n"
+    <<".Lchdir_fail:\n  mov $1, %edi\n.Lchdir_done:\n  mov $60, %eax\n  syscall\n"
+    <<".section .rodata\nchdir_path:\n  .asciz \".\"\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -3990,6 +4001,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_setsid_query_mode) { csubset::emit_setsid_query(program); return 0; }
     if(csubset::emit_umask_query_mode) { csubset::emit_umask_query(program); return 0; }
     if(csubset::emit_getcwd_query_mode) { csubset::emit_getcwd_query(program); return 0; }
+    if(csubset::emit_chdir_query_mode) { csubset::emit_chdir_query(program); return 0; }
     if(csubset::emit_timerfd_settime_mode) { csubset::emit_timerfd_settime(program); return 0; }
     if(csubset::emit_signalfd4_mode) { csubset::emit_signalfd4(program); return 0; }
     if(csubset::emit_pidfd_getfd_mode) { csubset::emit_pidfd_getfd(program); return 0; }
---
+++ fixtures/chdir_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  chdir_query();
+  return 0;
+}
