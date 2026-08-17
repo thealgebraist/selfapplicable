1db1cd24c190cf8da991174b676562dda55225e9
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:33:43 +0200

    Add rt sigprocmask query primitive

---
+++ fixtures/rt_sigprocmask_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  rt_sigprocmask_query();
+  return 0;
+}
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -536,6 +536,7 @@ expect_status fixtures/rt_sigtimedwait_query.c 1
 expect_status fixtures/rt_sigqueueinfo_query.c 1
 expect_status fixtures/rt_tgsigqueueinfo_query.c 1
 expect_status fixtures/set_robust_list_query.c 1
+expect_status fixtures/rt_sigprocmask_query.c 0
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
