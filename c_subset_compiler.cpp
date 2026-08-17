2caf12fc55805db979f24c3f745998e82e985390
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 10:36:48 +0200

    Add io cancel query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -134,6 +134,7 @@ bool emit_io_setup_query_mode = false;
 bool emit_perf_event_open_query_mode = false;
 bool emit_io_destroy_query_mode = false;
 bool emit_io_submit_query_mode = false;
+bool emit_io_cancel_query_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -1843,6 +1844,7 @@ Program parse_main(std::string const& s) {
   emit_perf_event_open_query_mode=std::regex_search(body,std::regex(R"re(\bperf_event_open_query\s*\(\s*\)\s*;)re"));
   emit_io_destroy_query_mode=std::regex_search(body,std::regex(R"re(\bio_destroy_query\s*\(\s*\)\s*;)re"));
   emit_io_submit_query_mode=std::regex_search(body,std::regex(R"re(\bio_submit_query\s*\(\s*\)\s*;)re"));
+  emit_io_cancel_query_mode=std::regex_search(body,std::regex(R"re(\bio_cancel_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -2839,6 +2841,15 @@ void emit_io_submit_query(Program const&) {
     <<".Lio_submit_done:\n  mov $60, %eax\n  syscall\n";
 }
 
+void emit_io_cancel_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $209, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  xor %edx, %edx\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lio_cancel_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lio_cancel_done\n"
+    <<".Lio_cancel_fail:\n  mov $1, %edi\n"
+    <<".Lio_cancel_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -3224,6 +3235,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_perf_event_open_query_mode) { csubset::emit_perf_event_open_query(program); return 0; }
     if(csubset::emit_io_destroy_query_mode) { csubset::emit_io_destroy_query(program); return 0; }
     if(csubset::emit_io_submit_query_mode) { csubset::emit_io_submit_query(program); return 0; }
+    if(csubset::emit_io_cancel_query_mode) { csubset::emit_io_cancel_query(program); return 0; }
     if(csubset::emit_timerfd_settime_mode) { csubset::emit_timerfd_settime(program); return 0; }
     if(csubset::emit_signalfd4_mode) { csubset::emit_signalfd4(program); return 0; }
     if(csubset::emit_pidfd_getfd_mode) { csubset::emit_pidfd_getfd(program); return 0; }
---
+++ fixtures/io_cancel_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  io_cancel_query();
+  return 0;
+}
