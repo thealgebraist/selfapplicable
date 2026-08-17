97010dc1df2724af1e85aec9d0d3f4052bf78c1b
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:20:44 +0200

    Add stat query primitive

---
+++ fixtures/stat_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  stat_query();
+  return 0;
+}
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -432,6 +432,7 @@ expect_status fixtures/symlink_query.c 1
 expect_status fixtures/link_query.c 1
 expect_status fixtures/rename_query.c 1
 expect_status fixtures/access_query.c 0
+expect_status fixtures/stat_query.c 0
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
