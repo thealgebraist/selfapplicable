1aba4c016844c00057546ec75be89aa45e54b120
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:40:29 +0200

    Add futex wake query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -98,6 +98,7 @@ bool emit_eventfd_write_mode = false;
 bool emit_memfd_secret_query_mode = false;
 bool emit_rseq_query_mode = false;
 bool emit_futex_waitv_query_mode = false;
+bool emit_futex_wake_query_mode = false;
 bool emit_process_mrelease_query_mode = false;
 bool emit_cachestat_query_mode = false;
 bool emit_set_mempolicy_home_node_query_mode = false;
@@ -1997,6 +1998,7 @@ Program parse_main(std::string const& s) {
   emit_memfd_secret_query_mode=std::regex_search(body,std::regex(R"re(\bmemfd_secret_query\s*\(\s*\)\s*;)re"));
   emit_rseq_query_mode=std::regex_search(body,std::regex(R"re(\brseq_query\s*\(\s*\)\s*;)re"));
   emit_futex_waitv_query_mode=std::regex_search(body,std::regex(R"re(\bfutex_waitv_query\s*\(\s*\)\s*;)re"));
+  emit_futex_wake_query_mode=std::regex_search(body,std::regex(R"re(\bfutex_wake_query\s*\(\s*\)\s*;)re"));
   emit_process_mrelease_query_mode=std::regex_search(body,std::regex(R"re(\bprocess_mrelease_query\s*\(\s*\)\s*;)re"));
   emit_cachestat_query_mode=std::regex_search(body,std::regex(R"re(\bcachestat_query\s*\(\s*\)\s*;)re"));
   emit_set_mempolicy_home_node_query_mode=std::regex_search(body,std::regex(R"re(\bset_mempolicy_home_node_query\s*\(\s*\)\s*;)re"));
@@ -2893,6 +2895,15 @@ void emit_futex_waitv_query(Program const&) {
     <<"  xor %edi, %edi\n  jmp .Lfutex_waitv_done\n"
     <<".Lfutex_waitv_fail:\n  mov $1, %edi\n"
     <<".Lfutex_waitv_done:\n  mov $60, %eax\n  syscall\n";
+}
+
+void emit_futex_wake_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $202, %eax\n  lea futex_wake_word(%rip), %rdi\n  mov $1, %esi\n  xor %edx, %edx\n  xor %r10d, %r10d\n  xor %r8d, %r8d\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lfutex_wake_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lfutex_wake_done\n"
+    <<".Lfutex_wake_fail:\n  mov $1, %edi\n.Lfutex_wake_done:\n"
+    <<"  mov $60, %eax\n  syscall\n.bss\n.align 4\nfutex_wake_word:\n  .skip 4\n";
 }
 
 void emit_process_mrelease_query(Program const&) {
@@ -5289,6 +5300,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_memfd_secret_query_mode) { csubset::emit_memfd_secret_query(program); return 0; }
     if(csubset::emit_rseq_query_mode) { csubset::emit_rseq_query(program); return 0; }
     if(csubset::emit_futex_waitv_query_mode) { csubset::emit_futex_waitv_query(program); return 0; }
+    if(csubset::emit_futex_wake_query_mode) { csubset::emit_futex_wake_query(program); return 0; }
     if(csubset::emit_process_mrelease_query_mode) { csubset::emit_process_mrelease_query(program); return 0; }
     if(csubset::emit_cachestat_query_mode) { csubset::emit_cachestat_query(program); return 0; }
     if(csubset::emit_set_mempolicy_home_node_query_mode) { csubset::emit_set_mempolicy_home_node_query(program); return 0; }
