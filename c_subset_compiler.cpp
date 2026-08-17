daaacda3bbe562a2372aa81762c3313efe6085f5
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:08:16 +0200

    Add ioctl query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -165,6 +165,7 @@ bool emit_ppoll_query_mode = false;
 bool emit_select_query_mode = false;
 bool emit_poll_query_mode = false;
 bool emit_clock_adjtime_query_mode = false;
+bool emit_ioctl_query_mode = false;
 bool emit_clone3_query_mode = false;
 bool emit_userfaultfd_query_mode = false;
 bool emit_kcmp_query_mode = false;
@@ -2020,6 +2021,7 @@ Program parse_main(std::string const& s) {
   emit_select_query_mode=std::regex_search(body,std::regex(R"re(\bselect_query\s*\(\s*\)\s*;)re"));
   emit_poll_query_mode=std::regex_search(body,std::regex(R"re(\bpoll_query\s*\(\s*\)\s*;)re"));
   emit_clock_adjtime_query_mode=std::regex_search(body,std::regex(R"re(\bclock_adjtime_query\s*\(\s*\)\s*;)re"));
+  emit_ioctl_query_mode=std::regex_search(body,std::regex(R"re(\bioctl_query\s*\(\s*\)\s*;)re"));
   emit_clone3_query_mode=std::regex_search(body,std::regex(R"re(\bclone3_query\s*\(\s*\)\s*;)re"));
   emit_userfaultfd_query_mode=std::regex_search(body,std::regex(R"re(\buserfaultfd_query\s*\(\s*\)\s*;)re"));
   emit_kcmp_query_mode=std::regex_search(body,std::regex(R"re(\bkcmp_query\s*\(\s*\)\s*;)re"));
@@ -4411,6 +4413,14 @@ void emit_clock_adjtime_query(Program const&) {
     <<".Lclock_adjtime_fail:\n  mov $1, %edi\n.Lclock_adjtime_done:\n  mov $60, %eax\n  syscall\n";
 }
 
+void emit_ioctl_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $16, %eax\n  mov $1, %edi\n  xor %esi, %esi\n  xor %edx, %edx\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lioctl_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lioctl_done\n"
+    <<".Lioctl_fail:\n  mov $1, %edi\n.Lioctl_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -4841,6 +4851,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_select_query_mode) { csubset::emit_select_query(program); return 0; }
     if(csubset::emit_poll_query_mode) { csubset::emit_poll_query(program); return 0; }
     if(csubset::emit_clock_adjtime_query_mode) { csubset::emit_clock_adjtime_query(program); return 0; }
+    if(csubset::emit_ioctl_query_mode) { csubset::emit_ioctl_query(program); return 0; }
     if(csubset::emit_process_vm_writev_query_mode) { csubset::emit_process_vm_writev_query(program); return 0; }
     if(csubset::emit_clone3_query_mode) { csubset::emit_clone3_query(program); return 0; }
     if(csubset::emit_userfaultfd_query_mode) { csubset::emit_userfaultfd_query(program); return 0; }
---
+++ fixtures/ioctl_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  ioctl_query();
+  return 0;
+}
