2908d59a84a6982d510a0d01b45728fdb4f442e0
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:26:01 +0200

    Add mremap query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -189,6 +189,7 @@ bool emit_mlockall_query_mode = false;
 bool emit_munlock_query_mode = false;
 bool emit_madvise_query_mode = false;
 bool emit_mprotect_query_mode = false;
+bool emit_mremap_query_mode = false;
 bool emit_clone3_query_mode = false;
 bool emit_userfaultfd_query_mode = false;
 bool emit_kcmp_query_mode = false;
@@ -2068,6 +2069,7 @@ Program parse_main(std::string const& s) {
   emit_munlock_query_mode=std::regex_search(body,std::regex(R"re(\bmunlock_query\s*\(\s*\)\s*;)re"));
   emit_madvise_query_mode=std::regex_search(body,std::regex(R"re(\bmadvise_query\s*\(\s*\)\s*;)re"));
   emit_mprotect_query_mode=std::regex_search(body,std::regex(R"re(\bmprotect_query\s*\(\s*\)\s*;)re"));
+  emit_mremap_query_mode=std::regex_search(body,std::regex(R"re(\bmremap_query\s*\(\s*\)\s*;)re"));
   emit_clone3_query_mode=std::regex_search(body,std::regex(R"re(\bclone3_query\s*\(\s*\)\s*;)re"));
   emit_userfaultfd_query_mode=std::regex_search(body,std::regex(R"re(\buserfaultfd_query\s*\(\s*\)\s*;)re"));
   emit_kcmp_query_mode=std::regex_search(body,std::regex(R"re(\bkcmp_query\s*\(\s*\)\s*;)re"));
@@ -4686,6 +4688,15 @@ void emit_mprotect_query(Program const&) {
     <<"  mov $60, %eax\n  syscall\n";
 }
 
+void emit_mremap_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $25, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  xor %edx, %edx\n  xor %r10d, %r10d\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lmremap_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lmremap_done\n"
+    <<".Lmremap_fail:\n  mov $1, %edi\n.Lmremap_done:\n"
+    <<"  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -5140,6 +5151,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_munlock_query_mode) { csubset::emit_munlock_query(program); return 0; }
     if(csubset::emit_madvise_query_mode) { csubset::emit_madvise_query(program); return 0; }
     if(csubset::emit_mprotect_query_mode) { csubset::emit_mprotect_query(program); return 0; }
+    if(csubset::emit_mremap_query_mode) { csubset::emit_mremap_query(program); return 0; }
     if(csubset::emit_process_vm_writev_query_mode) { csubset::emit_process_vm_writev_query(program); return 0; }
     if(csubset::emit_clone3_query_mode) { csubset::emit_clone3_query(program); return 0; }
     if(csubset::emit_userfaultfd_query_mode) { csubset::emit_userfaultfd_query(program); return 0; }
---
+++ fixtures/mremap_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  mremap_query();
+  return 0;
+}
