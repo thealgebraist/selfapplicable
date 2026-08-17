380f5ada0cfadc81d208bb99d9c745b93f6cba9c
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 13:59:24 +0200

    Add renameat2 query primitive

--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -514,6 +514,7 @@ expect_status fixtures/unlinkat_query.c 1
 expect_status fixtures/newfstatat_query.c 1
 expect_status fixtures/fchownat_query.c 1
 expect_status fixtures/readlinkat_query.c 1
+expect_status fixtures/renameat2_query.c 1
 expect_status fixtures/landlock_add_rule_query.c 1
 expect_status fixtures/landlock_restrict_self_query.c 1
 expect_status fixtures/keyctl_query.c 1
