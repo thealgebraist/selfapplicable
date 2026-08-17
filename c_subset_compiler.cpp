84dc0f2946f80b6fba97eed316849d5f51a79877
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 10:46:23 +0200

    Add connect query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -150,6 +150,7 @@ bool emit_setsockopt_query_mode = false;
 bool emit_socket_query_mode = false;
 bool emit_bind_query_mode = false;
 bool emit_listen_query_mode = false;
+bool emit_connect_query_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -1875,6 +1876,7 @@ Program parse_main(std::string const& s) {
   emit_socket_query_mode=std::regex_search(body,std::regex(R"re(\bsocket_query\s*\(\s*\)\s*;)re"));
   emit_bind_query_mode=std::regex_search(body,std::regex(R"re(\bbind_query\s*\(\s*\)\s*;)re"));
   emit_listen_query_mode=std::regex_search(body,std::regex(R"re(\blisten_query\s*\(\s*\)\s*;)re"));
+  emit_connect_query_mode=std::regex_search(body,std::regex(R"re(\bconnect_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -3015,6 +3017,15 @@ void emit_listen_query(Program const&) {
     <<".Llisten_done:\n  mov $60, %eax\n  syscall\n";
 }
 
+void emit_connect_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $42, %eax\n  mov $-1, %edi\n  xor %esi, %esi\n  xor %edx, %edx\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lconnect_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lconnect_done\n"
+    <<".Lconnect_fail:\n  mov $1, %edi\n"
+    <<".Lconnect_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -3416,6 +3427,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_socket_query_mode) { csubset::emit_socket_query(program); return 0; }
     if(csubset::emit_bind_query_mode) { csubset::emit_bind_query(program); return 0; }
     if(csubset::emit_listen_query_mode) { csubset::emit_listen_query(program); return 0; }
+    if(csubset::emit_connect_query_mode) { csubset::emit_connect_query(program); return 0; }
     if(csubset::emit_timerfd_settime_mode) { csubset::emit_timerfd_settime(program); return 0; }
     if(csubset::emit_signalfd4_mode) { csubset::emit_signalfd4(program); return 0; }
     if(csubset::emit_pidfd_getfd_mode) { csubset::emit_pidfd_getfd(program); return 0; }
