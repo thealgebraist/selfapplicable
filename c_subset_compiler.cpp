285e9bbbe0adf0b4e8bec7ed6ad0289811d9e45b
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 09:40:00 +0200

    Add sched_getattr query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -71,6 +71,7 @@ bool emit_futex_wake_mode = false;
 bool emit_epoll_wait_mode = false;
 bool emit_futex_wait_mode = false;
 bool emit_timerfd_gettime_mode = false;
+bool emit_sched_getattr_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -1717,6 +1718,7 @@ Program parse_main(std::string const& s) {
   emit_epoll_wait_mode=std::regex_search(body,std::regex(R"re(\bepoll_wait_query\s*\(\s*\)\s*;)re"));
   emit_futex_wait_mode=std::regex_search(body,std::regex(R"re(\bfutex_wait_probe\s*\(\s*\)\s*;)re"));
   emit_timerfd_gettime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_gettime_query\s*\(\s*\)\s*;)re"));
+  emit_sched_getattr_mode=std::regex_search(body,std::regex(R"re(\bsched_getattr_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -2241,6 +2243,11 @@ void emit_timerfd_gettime(Program const&) {
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_get_fail\n  mov %eax, %r12d\n  mov $287, %eax\n  mov %r12d, %edi\n  lea timer_get_buf(%rip), %rsi\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_get_fail\n  xor %edi, %edi\n  jmp .Ltimer_get_done\n.Ltimer_get_fail:\n  mov $1, %edi\n.Ltimer_get_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_get_buf:\n  .skip 32\n";
 }
 
+void emit_sched_getattr(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  movl $48, sched_attr_buf(%rip)\n  mov $315, %eax\n  xor %edi, %edi\n  lea sched_attr_buf(%rip), %rsi\n  mov $48, %edx\n  xor %r10d, %r10d\n  syscall\n  test %eax, %eax\n  js .Lsched_attr_fail\n  xor %edi, %edi\n  jmp .Lsched_attr_done\n.Lsched_attr_fail:\n  mov $1, %edi\n.Lsched_attr_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\nsched_attr_buf:\n  .skip 128\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -2563,6 +2570,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_epoll_wait_mode) { csubset::emit_epoll_wait(program); return 0; }
     if(csubset::emit_futex_wait_mode) { csubset::emit_futex_wait(program); return 0; }
     if(csubset::emit_timerfd_gettime_mode) { csubset::emit_timerfd_gettime(program); return 0; }
+    if(csubset::emit_sched_getattr_mode) { csubset::emit_sched_getattr(program); return 0; }
     if(csubset::emit_timerfd_settime_mode) { csubset::emit_timerfd_settime(program); return 0; }
     if(csubset::emit_signalfd4_mode) { csubset::emit_signalfd4(program); return 0; }
     if(csubset::emit_pidfd_getfd_mode) { csubset::emit_pidfd_getfd(program); return 0; }
---
+++ fixtures/sched_getattr_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  sched_getattr_query();
+  return 0;
+}
