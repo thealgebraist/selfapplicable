5e13bdb5d6606ba20240a67f12b9c85401e5c34a
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:29:19 +0200

    Add inotify init query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -223,6 +223,7 @@ bool emit_timerfd_query_mode = false;
 bool emit_epoll_create_query_mode = false;
 bool emit_epoll_ctl_query_mode = false;
 bool emit_epoll_wait_query_mode = false;
+bool emit_inotify_init_query_mode = false;
 bool emit_timerfd_settime_mode = false;
 bool emit_signalfd4_mode = false;
 bool emit_pidfd_getfd_mode = false;
@@ -2021,6 +2022,7 @@ Program parse_main(std::string const& s) {
   emit_epoll_create_query_mode=std::regex_search(body,std::regex(R"re(\bepoll_create_query\s*\(\s*\)\s*;)re"));
   emit_epoll_ctl_query_mode=std::regex_search(body,std::regex(R"re(\bepoll_ctl_query\s*\(\s*\)\s*;)re"));
   emit_epoll_wait_query_mode=std::regex_search(body,std::regex(R"re(\bepoll_wait_query\s*\(\s*\)\s*;)re"));
+  emit_inotify_init_query_mode=std::regex_search(body,std::regex(R"re(\binotify_init_query\s*\(\s*\)\s*;)re"));
   emit_timerfd_settime_mode=std::regex_search(body,std::regex(R"re(\btimerfd_settime_query\s*\(\s*\)\s*;)re"));
   emit_signalfd4_mode=std::regex_search(body,std::regex(R"re(\bsignalfd4_query\s*\(\s*\)\s*;)re"));
   emit_pidfd_getfd_mode=std::regex_search(body,std::regex(R"re(\bpidfd_getfd_probe\s*\(\s*\)\s*;)re"));
@@ -3800,6 +3802,14 @@ void emit_epoll_wait_query(Program const&) {
     <<".bss\n.align 8\nepoll_wait_events:\n  .skip 16\n";
 }
 
+void emit_inotify_init_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $253, %eax\n  xor %edi, %edi\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Linotify_init_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Linotify_init_done\n"
+    <<".Linotify_init_fail:\n  mov $1, %edi\n.Linotify_init_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -4120,6 +4130,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_readahead_mode) { csubset::emit_readahead(program); return 0; }
     if(csubset::emit_futex_wake_mode) { csubset::emit_futex_wake(program); return 0; }
     if(csubset::emit_epoll_wait_query_mode) { csubset::emit_epoll_wait_query(program); return 0; }
+    if(csubset::emit_inotify_init_query_mode) { csubset::emit_inotify_init_query(program); return 0; }
     if(csubset::emit_epoll_wait_mode) { csubset::emit_epoll_wait(program); return 0; }
     if(csubset::emit_futex_wait_mode) { csubset::emit_futex_wait(program); return 0; }
     if(csubset::emit_timerfd_gettime_mode) { csubset::emit_timerfd_gettime(program); return 0; }
---
+++ fixtures/inotify_init_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  inotify_init_query();
+  return 0;
+}
