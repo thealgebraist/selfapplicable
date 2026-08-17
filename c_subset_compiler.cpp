5ad62c347f6b2ee3a89becb344adfaa440e50cd0
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:48:45 +0200

    Add getrusage query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -138,6 +138,7 @@ bool emit_sched_setaffinity_query_mode = false;
 bool emit_sched_getcpu_query_mode = false;
 bool emit_getpriority_query_mode = false;
 bool emit_getrlimit_query_mode = false;
+bool emit_getrusage_query_mode = false;
 bool emit_clone3_query_mode = false;
 bool emit_userfaultfd_query_mode = false;
 bool emit_kcmp_query_mode = false;
@@ -1966,6 +1967,7 @@ Program parse_main(std::string const& s) {
   emit_sched_getcpu_query_mode=std::regex_search(body,std::regex(R"re(\bsched_getcpu_query\s*\(\s*\)\s*;)re"));
   emit_getpriority_query_mode=std::regex_search(body,std::regex(R"re(\bgetpriority_query\s*\(\s*\)\s*;)re"));
   emit_getrlimit_query_mode=std::regex_search(body,std::regex(R"re(\bgetrlimit_query\s*\(\s*\)\s*;)re"));
+  emit_getrusage_query_mode=std::regex_search(body,std::regex(R"re(\bgetrusage_query\s*\(\s*\)\s*;)re"));
   emit_clone3_query_mode=std::regex_search(body,std::regex(R"re(\bclone3_query\s*\(\s*\)\s*;)re"));
   emit_userfaultfd_query_mode=std::regex_search(body,std::regex(R"re(\buserfaultfd_query\s*\(\s*\)\s*;)re"));
   emit_kcmp_query_mode=std::regex_search(body,std::regex(R"re(\bkcmp_query\s*\(\s*\)\s*;)re"));
@@ -4110,6 +4112,15 @@ void emit_getrlimit_query(Program const&) {
     <<".bss\n.align 8\ngetrlimit_value:\n  .skip 16\n";
 }
 
+void emit_getrusage_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $98, %eax\n  mov $0, %edi\n  lea getrusage_value(%rip), %rsi\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lgetrusage_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lgetrusage_done\n"
+    <<".Lgetrusage_fail:\n  mov $1, %edi\n.Lgetrusage_done:\n  mov $60, %eax\n  syscall\n"
+    <<".bss\n.align 8\ngetrusage_value:\n  .skip 144\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -4513,6 +4524,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_sched_getcpu_query_mode) { csubset::emit_sched_getcpu_query(program); return 0; }
     if(csubset::emit_getpriority_query_mode) { csubset::emit_getpriority_query(program); return 0; }
     if(csubset::emit_getrlimit_query_mode) { csubset::emit_getrlimit_query(program); return 0; }
+    if(csubset::emit_getrusage_query_mode) { csubset::emit_getrusage_query(program); return 0; }
     if(csubset::emit_process_vm_writev_query_mode) { csubset::emit_process_vm_writev_query(program); return 0; }
     if(csubset::emit_clone3_query_mode) { csubset::emit_clone3_query(program); return 0; }
     if(csubset::emit_userfaultfd_query_mode) { csubset::emit_userfaultfd_query(program); return 0; }
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -360,6 +360,7 @@ expect_status fixtures/sched_setaffinity_query.c 1
 expect_status fixtures/sched_getcpu_query.c 0
 expect_status fixtures/getpriority_query.c 0
 expect_status fixtures/getrlimit_query.c 0
+expect_status fixtures/getrusage_query.c 0
 expect_status fixtures/process_vm_writev_query.c 0
 expect_status fixtures/clone3_query.c 1
 expect_status fixtures/userfaultfd_query.c 1
