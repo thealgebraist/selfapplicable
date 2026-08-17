c64741bada6ee2dcab2dad4cb100ead9d2b186ff
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:10:23 +0200

    Add setuid and setgid query primitives

--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -410,6 +410,8 @@ expect_status fixtures/setreuid_query.c 0
 expect_status fixtures/setregid_query.c 0
 expect_status fixtures/getresgid_query.c 0
 expect_status fixtures/getresuid_query.c 0
+expect_status fixtures/setuid_query.c 1
+expect_status fixtures/setgid_query.c 1
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
