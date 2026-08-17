e76162d829c12e47b93c163e71be6461ff46b875
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:16:12 +0200

    Add fcntl setlease query primitive

--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -398,6 +398,7 @@ expect_status fixtures/fcntl_setown_query.c 0
 expect_status fixtures/fcntl_setfl_query.c 0
 expect_status fixtures/fcntl_getlease_query.c 0
 expect_status fixtures/fcntl_getseal_query.c 1
+expect_status fixtures/fcntl_setlease_query.c 1
 expect_status fixtures/process_vm_writev_query.c 0
 expect_status fixtures/clone3_query.c 1
 expect_status fixtures/userfaultfd_query.c 1
