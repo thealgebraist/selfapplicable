b56ead59328913686336bfd4e7135c1c02bf7d87
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 12:06:56 +0200

    Add poll query primitive

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -163,6 +163,7 @@ bool emit_fstatfs_query_mode = false;
 bool emit_pselect6_query_mode = false;
 bool emit_ppoll_query_mode = false;
 bool emit_select_query_mode = false;
+bool emit_poll_query_mode = false;
 bool emit_clone3_query_mode = false;
 bool emit_userfaultfd_query_mode = false;
 bool emit_kcmp_query_mode = false;
@@ -2016,6 +2017,7 @@ Program parse_main(std::string const& s) {
   emit_pselect6_query_mode=std::regex_search(body,std::regex(R"re(\bpselect6_query\s*\(\s*\)\s*;)re"));
   emit_ppoll_query_mode=std::regex_search(body,std::regex(R"re(\bppoll_query\s*\(\s*\)\s*;)re"));
   emit_select_query_mode=std::regex_search(body,std::regex(R"re(\bselect_query\s*\(\s*\)\s*;)re"));
+  emit_poll_query_mode=std::regex_search(body,std::regex(R"re(\bpoll_query\s*\(\s*\)\s*;)re"));
   emit_clone3_query_mode=std::regex_search(body,std::regex(R"re(\bclone3_query\s*\(\s*\)\s*;)re"));
   emit_userfaultfd_query_mode=std::regex_search(body,std::regex(R"re(\buserfaultfd_query\s*\(\s*\)\s*;)re"));
   emit_kcmp_query_mode=std::regex_search(body,std::regex(R"re(\bkcmp_query\s*\(\s*\)\s*;)re"));
@@ -4391,6 +4393,14 @@ void emit_select_query(Program const&) {
     <<".section .data\n.align 8\nselect_timeout:\n  .quad 0\n  .quad 0\n";
 }
 
+void emit_poll_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $7, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lpoll_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lpoll_done\n"
+    <<".Lpoll_fail:\n  mov $1, %edi\n.Lpoll_done:\n  mov $60, %eax\n  syscall\n";
+}
+
 void emit_timerfd_settime(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $283, %eax\n  xor %edi, %edi\n  xor %esi, %esi\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  mov %eax, %r12d\n  mov $286, %eax\n  mov %r12d, %edi\n  xor %esi, %esi\n  lea timer_set_new(%rip), %rdx\n  xor %r10d, %r10d\n  syscall\n  mov %r12d, %edi\n  mov $3, %eax\n  syscall\n  test %eax, %eax\n  js .Ltimer_set_fail\n  xor %edi, %edi\n  jmp .Ltimer_set_done\n.Ltimer_set_fail:\n  mov $1, %edi\n.Ltimer_set_done:\n  mov $60, %eax\n  syscall\n.bss\n.align 8\ntimer_set_new:\n  .skip 32\n";
@@ -4819,6 +4829,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_pselect6_query_mode) { csubset::emit_pselect6_query(program); return 0; }
     if(csubset::emit_ppoll_query_mode) { csubset::emit_ppoll_query(program); return 0; }
     if(csubset::emit_select_query_mode) { csubset::emit_select_query(program); return 0; }
+    if(csubset::emit_poll_query_mode) { csubset::emit_poll_query(program); return 0; }
     if(csubset::emit_process_vm_writev_query_mode) { csubset::emit_process_vm_writev_query(program); return 0; }
     if(csubset::emit_clone3_query_mode) { csubset::emit_clone3_query(program); return 0; }
     if(csubset::emit_userfaultfd_query_mode) { csubset::emit_userfaultfd_query(program); return 0; }
---
+++ fixtures/poll_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  poll_query();
+  return 0;
+}
