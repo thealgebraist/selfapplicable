b20ee211e1e353578305a87a118010d7f8a08604
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 10:14:07 +0200

    Add process mrelease query primitive

--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -321,6 +321,7 @@ expect_status fixtures/eventfd_write_query.c 0
 expect_status fixtures/memfd_secret_query.c 0
 expect_status fixtures/rseq_query.c 1
 expect_status fixtures/futex_waitv_query.c 1
+expect_status fixtures/process_mrelease_query.c 1
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
