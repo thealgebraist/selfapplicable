88d3d5b922df43ca8c85c8b8932a7a8fda9fc114
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:40:04 +0200

    Add clock gettime query primitive

---
+++ fixtures/clock_gettime_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  clock_gettime_query();
+  return 0;
+}
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -346,6 +346,7 @@ expect_status fixtures/getrandom_query.c 0
 expect_status fixtures/sched_yield_query.c 0
 expect_status fixtures/nanosleep_query.c 1
 expect_status fixtures/clock_nanosleep_query.c 1
+expect_status fixtures/clock_gettime_query.c 0
 expect_status fixtures/process_vm_writev_query.c 0
 expect_status fixtures/clone3_query.c 1
 expect_status fixtures/userfaultfd_query.c 1
