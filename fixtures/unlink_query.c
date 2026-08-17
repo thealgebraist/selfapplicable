e8a98af853af2e27ada2dfcba71ded591a401218
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:17:25 +0200

    Add unlink query primitive

---
+++ fixtures/unlink_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  unlink_query();
+  return 0;
+}
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -426,6 +426,7 @@ expect_status fixtures/chdir_query.c 0
 expect_status fixtures/fchdir_query.c 1
 expect_status fixtures/mkdir_query.c 1
 expect_status fixtures/rmdir_query.c 1
+expect_status fixtures/unlink_query.c 1
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
