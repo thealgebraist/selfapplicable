c0677ea2c443a641817fb4a6a334f64fbb102b38
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 11:30:27 +0200

    Add memfd query primitive

---
+++ fixtures/memfd_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  memfd_query();
+  return 0;
+}
--- test_c_subset_assembler.sh
+++ test_c_subset_assembler.sh
@@ -448,6 +448,7 @@ expect_status fixtures/epoll_ctl_query.c 1
 expect_status fixtures/epoll_wait_query.c 1
 expect_status fixtures/inotify_init_query.c 0
 expect_status fixtures/inotify_add_watch_query.c 1
+expect_status fixtures/memfd_query.c 0
 expect_status fixtures/timerfd_settime_query.c 0
 expect_status fixtures/signalfd4_query.c 0
 expect_status fixtures/pidfd_getfd_probe.c 1
