f0479b810224e53ecee4059ea26a75ef6b86ca5b
The Algebraist <thealgebraist@users.noreply.github.com>
2026-08-17 15:10:09 +0200

    Add setpriority syscall query

--- c_subset_compiler.cpp
+++ c_subset_compiler.cpp
@@ -171,6 +171,7 @@ bool emit_sched_getaffinity_query_mode = false;
 bool emit_sched_setaffinity_query_mode = false;
 bool emit_sched_getcpu_query_mode = false;
 bool emit_getpriority_query_mode = false;
+bool emit_setpriority_query_mode = false;
 bool emit_getrlimit_query_mode = false;
 bool emit_getrusage_query_mode = false;
 bool emit_getxattr_query_mode = false;
@@ -2155,6 +2156,7 @@ Program parse_main(std::string const& s) {
   emit_sched_setaffinity_query_mode=std::regex_search(body,std::regex(R"re(\bsched_setaffinity_query\s*\(\s*\)\s*;)re"));
   emit_sched_getcpu_query_mode=std::regex_search(body,std::regex(R"re(\bsched_getcpu_query\s*\(\s*\)\s*;)re"));
   emit_getpriority_query_mode=std::regex_search(body,std::regex(R"re(\bgetpriority_query\s*\(\s*\)\s*;)re"));
+  emit_setpriority_query_mode=std::regex_search(body,std::regex(R"re(\bsetpriority_query\s*\(\s*\)\s*;)re"));
   emit_getrlimit_query_mode=std::regex_search(body,std::regex(R"re(\bgetrlimit_query\s*\(\s*\)\s*;)re"));
   emit_getrusage_query_mode=std::regex_search(body,std::regex(R"re(\bgetrusage_query\s*\(\s*\)\s*;)re"));
   emit_getxattr_query_mode=std::regex_search(body,std::regex(R"re(\bgetxattr_query\s*\(\s*\)\s*;)re"));
@@ -5297,6 +5299,15 @@ void emit_getpriority_query(Program const&) {
     <<".Lgetpriority_fail:\n  mov $1, %edi\n.Lgetpriority_done:\n  mov $60, %eax\n  syscall\n";
 }
 
+void emit_setpriority_query(Program const&) {
+  std::cout<<".text\n.globl _start\n_start:\n"
+    <<"  mov $141, %eax\n  xor %edi, %edi\n  mov $-1, %esi\n  xor %edx, %edx\n  syscall\n"
+    <<"  test %eax, %eax\n  js .Lsetpriority_fail\n"
+    <<"  xor %edi, %edi\n  jmp .Lsetpriority_done\n"
+    <<".Lsetpriority_fail:\n  mov $1, %edi\n.Lsetpriority_done:\n"
+    <<"  mov $60, %eax\n  syscall\n";
+}
+
 void emit_getrlimit_query(Program const&) {
   std::cout<<".text\n.globl _start\n_start:\n"
     <<"  mov $97, %eax\n  xor %edi, %edi\n  lea getrlimit_value(%rip), %rsi\n  syscall\n"
@@ -6295,6 +6306,7 @@ int main(int argc,char **argv) {
     if(csubset::emit_sched_setaffinity_query_mode) { csubset::emit_sched_setaffinity_query(program); return 0; }
     if(csubset::emit_sched_getcpu_query_mode) { csubset::emit_sched_getcpu_query(program); return 0; }
     if(csubset::emit_getpriority_query_mode) { csubset::emit_getpriority_query(program); return 0; }
+    if(csubset::emit_setpriority_query_mode) { csubset::emit_setpriority_query(program); return 0; }
     if(csubset::emit_getrlimit_query_mode) { csubset::emit_getrlimit_query(program); return 0; }
     if(csubset::emit_getrusage_query_mode) { csubset::emit_getrusage_query(program); return 0; }
     if(csubset::emit_getxattr_query_mode) { csubset::emit_getxattr_query(program); return 0; }
---
+++ fixtures/setpriority_query.c
@@ -0,0 +1,4 @@
+int main(int argc, char **argv) {
+  setpriority_query();
+  return 0;
+}
